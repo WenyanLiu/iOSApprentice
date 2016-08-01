57e5f4ba854ffea85847880f3b805655997756c2
Rockie <f.v.rockie@hotmail.com>
2016-08-01 22:24:38 +0800

    + LOLHero init

---
+++ LOLHero/LOLHero/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOLHero
+//
+//  Created by 刘文炎 on 8/1/16.
+//  Copyright © 2016 F.V.Rockie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
