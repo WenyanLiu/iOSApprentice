f116dfc0d87edace0111dcf8bbb11ae7da3110e2
Rockie <f.v.rockie@hotmail.com>
2016-08-04 21:46:43 +0800

    + FacePlusPlus

---
+++ FacePlusPlus/FacePlusPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacePlusPlus
+//
+//  Created by 刘文炎 on 8/4/16.
+//  Copyright © 2016 F.V.Rockie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacePlusPlus/FacePlusPlus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FacePlusPlus
+//
+//  Created by 刘文炎 on 8/4/16.
+//  Copyright © 2016 F.V.Rockie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
